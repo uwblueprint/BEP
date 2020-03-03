125d5020a3fd53215ea4c348f199ab6739d6614d
Emma Lozhkin <elozhkin@uwaterloo.ca>
2020-03-02 20:11:02 -0500

    index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,16 @@
 import math from './mathematic';
+import express from 'express';
+import bodyParser from 'body-parser';
+import { userInfo } from 'os';
 
-console.log('The answer is: ' + math.add(2, 2));
+// console.log('The answer is: ' + math.add(2, 2));
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// app.get('api/users/:id', getUser);
+
+app.listen(port, () => console.log("Hello world"));
