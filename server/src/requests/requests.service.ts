9961464aec82c3c66bf61d5c211bdbbb0fefc2b8
Sara Fong <sara.fong@uwaterloo.ca>
2020-05-14 22:21:59 -0400

    code cleanup

--- server/src/requests/requests.service.ts
+++ server/src/requests/requests.service.ts
@@ -27,20 +27,6 @@ const requests: Requests = {
     },
 }
 
-/**
- * OAuth Token verification
- */
-
-//  //Verify session here 
-//  const oauth2 = new jsforce.OAuth2({
-//     loginUrl: process.env.LOGIN_URL,
-//     clientId: process.env.CLIENT_ID,
-//     clientSecret: process.env.CLIENT_SECRET,
-//     redirectUri: 'http://localhost:3030/token'
-// });
-
-// var conn = new jsforce.Connection({ oauth2 : oauth2 });
-
 
 /**
  * Static variables 
