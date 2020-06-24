c033ccdc471d39c183f3983729d5eb35226fd793
Faizaan Madhani <fzmadhani@gmail.com>
2020-06-24 19:27:32 -0400

    Fixed type definitions

--- server/src/middleware/jwt.ts
+++ server/src/middleware/jwt.ts
@@ -3,7 +3,8 @@ import * as jwt from "jsonwebtoken";
 
 export const verifyWebToken = () => {
     return function(req: Express.Request, res: Express.Response, next: Function) {
-        const token = req.headers["token"]
+        let token: string;
+        token = req.headers["token"].toString()
         console.log(token)
         jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
             if (!err) {
