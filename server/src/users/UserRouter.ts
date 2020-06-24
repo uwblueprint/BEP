7e73f55e130d8b87fc7a7809bfe09d7ed381ee49
Faizaan Madhani <fzmadhani@gmail.com>
2020-06-24 18:40:30 -0400

    Removed create route, moved login/signup to authRouter

--- server/src/users/UserRouter.ts
+++ server/src/users/UserRouter.ts
@@ -28,42 +28,20 @@ export const userRouter = Express.Router();
 
 // GET requests/:id currently using the Name field (first name) since don't know how to actually grab object id in salesforce
 
-// userRouter.get("/:name", async (req: Express.Request, res: Express.Response) => {
-//     // const id: number = parseInt(req.params.id, 10);
-//     const name: string = req.params.name;
+userRouter.get("/:name", async (req: Express.Request, res: Express.Response) => {
+    // const id: number = parseInt(req.params.id, 10);
+    const name: string = req.params.name;
 
-//     try {
-//         const fetchedUser = await UserService.getUserInfo(name);
+    try {
+        const fetchedUser = await UserService.getUserInfo(name);
 
-//         res.status(200).send(fetchedUser);
-//     } catch (e) {
-//         res.status(404).send(e.message);
-//     }
-// });
+        res.status(200).send(fetchedUser);
+    } catch (e) {
+        res.status(404).send(e.message);
+    }
+});
 
-// // POST requests/
-
-// userRouter.post("/create", async (req: Express.Request, res: Express.Response) => {
-//     try {
-//         // Type match request body into User interface when Salesforce fields are figured out
-//         // const userInfo: User = req.body.user;
-//         let name: string = req.body.name;
-//         let email: string = req.body.email;
-//         let password: string = req.body.password;
-//         let phoneNumber: string = req.body.phoneNumber;
-//         let lastName: string = req.body.lastName;
-//         await UserService.create(name, email, password, phoneNumber, lastName);
-
-//         res.sendStatus(201);
-//     } catch (e) {
-//         res.status(404).send(e.message);
-//     }
-// });
-
-// // PUT requests/
-
-
-// // DELETE requests/:id using name for now
+// DELETE Request
 
 userRouter.delete("/:name", async (req: Express.Request, res: Express.Response) => {
     try {
@@ -75,73 +53,5 @@ userRouter.delete("/:name", async (req: Express.Request, res: Express.Response) 
         res.status(500).send(e.message);
     }
 }); 
-
-userRouter.post("/register", async (req: Express.Request, res: Express.Response) => {
-    try {
-        let firstName: string = req.body.firstName
-        let email: string = req.body.email;
-        let password: string = req.body.password;
-        let phoneNumber: string = req.body.phoneNumber;
-        let lastName: string = req.body.lastName;
-        let personalPronouns: string = req.body.personalPronouns
-        let userType: string = req.body.userType
-
-
-        //Hash Password
-        const hash = await bcrypt.hash(password, BCRYPT_ROUNDS)
-        
-        await UserService.create(firstName + " " + lastName, firstName, email, hash, phoneNumber, lastName, personalPronouns, userType).then(response => {
-
-            if (response["success"] == true) {
-                let token = jwt.sign({email: email}, process.env.SECRET_KEY, {expiresIn: 120});
-                res.status(200).send({
-                    "data": response,
-                    "token": token
-                });
-
-            }
-        });
-
-
-    } catch (e) {
-        res.status(404).send(e.message)
-    }
-});
-
-userRouter.post('/login', async (req: Express.Request, res: Express.Response) => {
-    try {
-        let email: string = req.body.email;
-        let password: string = req.body.password;
-
-        const fetchUser = await UserService.getUserInfo(email)
-        let valid = false
-        if (fetchUser != undefined) {
-            valid = await bcrypt.compare(password, fetchUser.password)
-            console.log("The comparison was:" + valid)
-        }
-
-        if (valid) {
-            res.status(200).send({
-                "Status":"Authenticated"
-            })
-        } else {
-            res.status(401).send({
-                "Status":"Unauthenticated",
-                "Message":"Incorrect Username or Password"
-            }) 
-        }
-
-
-    } catch (e) {
-        console.error(e)
-    }
-});
-
-//Test UserRouter for Sanity Checks
-userRouter.get("/hello_world", async (req: Express.Request, res: Express.Response) => {
-    res.json({"message" : "hello World"});
-});
-
-userRouter
 
 
