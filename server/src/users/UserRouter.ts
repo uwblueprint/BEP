c446693beb7a9808b388e413bf236d070e42f66f
Megan Niu <megan.niu@gmail.com>
2020-07-05 22:59:59 -0400

    feat(user_api): add endpoint to get volunteer list (#52)

--- server/src/users/UserRouter.ts
+++ server/src/users/UserRouter.ts
@@ -2,8 +2,8 @@
  * Required External Modules and Interfaces
  */
 
-import * as Express from "express";
-import * as UserService from "./UserService";
+import * as UserService from './UserService';
+import * as Express from 'express';
 
 /**
  * Router Definition
@@ -15,32 +15,65 @@ export const userRouter = Express.Router();
  * Controller Definitions
  */
 
-// GET requests/:id currently using the Name field (first name) since don't know how to actually grab object id in salesforce
+// GET users/?email&type&limit&offset
 
-userRouter.get("/:name", async (req: Express.Request, res: Express.Response) => {
-    // const id: number = parseInt(req.params.id, 10);
-    const name: string = req.params.name;
+userRouter.get('/', async (req: Express.Request, res: Express.Response) => {
+    const email: string = req.query.email as string;
+    const type: string = req.query.type as string;
+    const limit: number = req.query.limit as any;
+    const offset: number = req.query.offset as any;
 
     try {
-        const fetchedUser = await UserService.getUser(name);
-
-        res.status(200).send(fetchedUser);
+        if (email !== undefined) {
+            // If email provided in query parameters, return user with email address.
+            const fetchedUser = await UserService.getUser(email);
+            res.status(200).send(fetchedUser);
+        } else if (type === 'volunteer') {
+            // If query parameters specify "volunteer", then return a paginated list of volunteers.
+            const users = await UserService.getVolunteers(limit ? limit : 10, offset ? offset : 0);
+            res.status(200).send(users);
+        } else {
+            throw Error(`Invalid query parameters. Either set "email" parameter or set "type" parameter.`);
+        }
     } catch (e) {
-        res.status(404).send(e.message);
+        res.status(500).send({ msg: e.message });
     }
 });
 
-// DELETE Request
+// POST users/
 
-userRouter.delete("/:name", async (req: Express.Request, res: Express.Response) => {
+userRouter.post('/', async (req: Express.Request, res: Express.Response) => {
+    try {
+        const id: string = await UserService.create(req.body);
+
+        res.status(201).send({ id });
+    } catch (e) {
+        res.status(500).send({ msg: e.message });
+    }
+});
+
+// PUT users/:id
+
+userRouter.put('/:id', async (req: Express.Request, res: Express.Response) => {
+    try {
+        const id: string = req.params.id;
+        await UserService.update(id, req.body);
+
+        res.status(200).send({ id: id });
+    } catch (e) {
+        res.status(500).send({ msg: e.message });
+    }
+});
+
+// DELETE users/:id
+
+userRouter.delete('/:id', async (req: Express.Request, res: Express.Response) => {
     try {
         let id: string = req.params.id;
         await UserService.remove(id);
 
         res.sendStatus(200);
     } catch (e) {
-        res.status(500).send(e.message);
+        res.status(500).send({ msg: e.message });
     }
 });
-
-
--- server/src/users/UserService.ts
+++ server/src/users/UserService.ts
@@ -8,14 +8,21 @@ import User, { isUser, UserType } from './UserInterface';
 import Volunteer, { isVolunteer } from './VolunteerInterface';
 
 const siteUser: string = 'SiteUser__c';
+const userFields: string =
+    'email__c, firstName__c, phoneNumber__c, followedPrograms__c, Id, isSubscribed__c, lastName__c, password__c, ' +
+    'preferredPronouns__c, userType__c, educatorDesiredActivities__c, position__c, schoolBoard__c, school__c, careerDescription__c, ' +
+    'coopPlacementMode__c, coopPlacementSchoolAffiliation__c, coopPlacementTime__c, jobTitle__c, department__c, employerName__c, ' +
+    'employmentStatus__c, expertiseAreas__c, extraDescription__c, grades__c, introductionMethod__c, isVolunteerCoordinator__c, languages__c, ' +
+    'linkedIn__c, localPostSecondaryInstitutions__c, locations__c, postSecondaryTraining__c, professionalAssociations__c, reasonsForVolunteering__c,' +
+    'volunteerDesiredExternalActivities__c, volunteerDesiredInternalActivities__c';
 
 // Converts between Salesforce picklist (string of object delimited by semicolons) and arrays.
 const arrayToPicklistString = (arr: string[]): string =>
     arr.reduce((acc, item) => acc + item + ';', '').replace(/;$/, ''); // Remove trailing semicolon.
-const picklistStringToArray = (str: string): string[] => str.split(';');
+const picklistStringToArray = (str: string): string[] => (str ? str.split(';') : []);
 
-// Map fields of user modes to Salesforce fields.
-const createSalesforceUser = (user: User, id?: string): any => {
+// Map fields of user model to Salesforce fields.
+const userModelToSalesforceUser = (user: User, id?: string): any => {
     let salesforceUser: any = {
         email__c: user.email,
         firstName__c: user.firstName,
@@ -83,6 +90,62 @@ const createSalesforceUser = (user: User, id?: string): any => {
     return salesforceUser;
 };
 
+// Map Saleforce record fields to user model fields.
+const salesforceUserToUserModel = (record: any): User => {
+    const user: User = {
+        email: record.email__c,
+        firstName: record.firstName__c,
+        followedPrograms: record.followedPrograms__c,
+        id: record.Id,
+        isSubscribed: record.isSubscribed__c,
+        lastName: record.lastName__c,
+        password: record.password__c,
+        phoneNumber: record.phoneNumber__c,
+        preferredPronouns: record.preferredPronouns__c,
+        userType: record.userType__c
+    };
+
+    if (UserType[record.userType__c] === UserType[UserType.Educator]) {
+        // User is an educator.
+        (user as Educator).educatorDesiredActivities = picklistStringToArray(record.educatorDesiredActivities__c);
+        (user as Educator).position = record.position__c;
+        (user as Educator).schoolBoard = record.schoolBoard__c;
+        (user as Educator).school = record.school__c;
+    } else if (UserType[record.userType__c] === UserType[UserType.Volunteer]) {
+        // User is a volunteer.
+        (user as Volunteer).careerDescription = record.careerDescription__c;
+        (user as Volunteer).coopPlacementMode = record.coopPlacementMode__c;
+        (user as Volunteer).coopPlacementSchoolAffiliation = record.coopPlacementSchoolAffiliation__c;
+        (user as Volunteer).coopPlacementTime = picklistStringToArray(record.coopPlacementTime__c);
+        (user as Volunteer).jobTitle = record.jobTitle__c;
+        (user as Volunteer).department = record.department__c;
+        (user as Volunteer).employerName = record.employerName__c;
+        (user as Volunteer).employmentStatus = record.employmentStatus__c;
+        (user as Volunteer).expertiseAreas = picklistStringToArray(record.expertiseAreas__c);
+        (user as Volunteer).extraDescription = record.extraDescription__c;
+        (user as Volunteer).grades = picklistStringToArray(record.grades__c);
+        (user as Volunteer).introductionMethod = record.introductionMethod__c;
+        (user as Volunteer).isVolunteerCoordinator = record.isVolunteerCoordinator__c;
+        (user as Volunteer).languages = picklistStringToArray(record.languages__c);
+        (user as Volunteer).linkedIn = record.linkedIn__c;
+        (user as Volunteer).localPostSecondaryInstitutions = picklistStringToArray(
+            record.localPostSecondaryInstitutions__c
+        );
+        (user as Volunteer).locations = picklistStringToArray(record.locations__c);
+        (user as Volunteer).postSecondaryTraining = picklistStringToArray(record.postSecondaryTraining__c);
+        (user as Volunteer).professionalAssociations = picklistStringToArray(record.professionalAssociations__c);
+        (user as Volunteer).reasonsForVolunteering = picklistStringToArray(record.reasonsForVolunteering__c);
+        (user as Volunteer).volunteerDesiredExternalActivities = picklistStringToArray(
+            record.volunteerDesiredExternalActivities__c
+        );
+        (user as Volunteer).volunteerDesiredInternalActivities = picklistStringToArray(
+            record.volunteerDesiredInternalActivities__c
+        );
+    }
+
+    return user;
+};
+
 /**
  * Service Methods
  */
@@ -93,14 +156,6 @@ const createSalesforceUser = (user: User, id?: string): any => {
 
 // Retrieve user by ID.
 export const getUser = async (email: string): Promise<User> => {
-    const userFields =
-        'email__c, firstName__c, phoneNumber__c, followedPrograms__c, Id, isSubscribed__c, lastName__c, password__c, ' +
-        'preferredPronouns__c, userType__c, educatorDesiredActivities__c, position__c, schoolBoard__c, school__c, careerDescription__c, ' +
-        'coopPlacementMode__c, coopPlacementSchoolAffiliation__c, coopPlacementTime__c, jobTitle__c, department__c, employerName__c, ' +
-        'employmentStatus__c, expertiseAreas__c, extraDescription__c, grades__c, introductionMethod__c, isVolunteerCoordinator__c, languages__c, ' +
-        'linkedIn__c, localPostSecondaryInstitutions__c, locations__c, postSecondaryTraining__c, professionalAssociations__c, reasonsForVolunteering__c,' +
-        'volunteerDesiredExternalActivities__c, volunteerDesiredInternalActivities__c';
-
     const user: User = conn
         .sobject(siteUser)
         .find(
@@ -114,65 +169,28 @@ export const getUser = async (email: string): Promise<User> => {
             if (err) {
                 return console.error(err);
             }
-
-            const user: User = {
-                email: record[0].email__c,
-                firstName: record[0].firstName__c,
-                followedPrograms: record[0].followedPrograms__c,
-                id: record[0].Id,
-                isSubscribed: record[0].isSubscribed__c,
-                lastName: record[0].lastName__c,
-                password: record[0].password__c,
-                phoneNumber: record[0].phoneNumber__c,
-                preferredPronouns: record[0].preferredPronouns__c,
-                userType: record[0].userType__c
-            };
-
-            if (UserType[record[0].userType__c] === UserType[UserType.Educator]) {
-                // User is an educator.
-                (user as Educator).educatorDesiredActivities = picklistStringToArray(
-                    record[0].educatorDesiredActivities__c
-                );
-                (user as Educator).position = record[0].position__c;
-                (user as Educator).schoolBoard = record[0].schoolBoard__c;
-                (user as Educator).school = record[0].school__c;
-            } else if (UserType[record[0].userType__c] === UserType[UserType.Volunteer]) {
-                // User is a volunteer.
-                (user as Volunteer).careerDescription = record[0].careerDescription__c;
-                (user as Volunteer).coopPlacementMode = record[0].coopPlacementMode__c;
-                (user as Volunteer).coopPlacementSchoolAffiliation = record[0].coopPlacementSchoolAffiliation__c;
-                (user as Volunteer).coopPlacementTime = picklistStringToArray(record[0].coopPlacementTime__c);
-                (user as Volunteer).jobTitle = record[0].jobTitle__c;
-                (user as Volunteer).department = record[0].department__c;
-                (user as Volunteer).employerName = record[0].employerName__c;
-                (user as Volunteer).employmentStatus = record[0].employmentStatus__c;
-                (user as Volunteer).expertiseAreas = picklistStringToArray(record[0].expertiseAreas__c);
-                (user as Volunteer).extraDescription = record[0].extraDescription__c;
-                (user as Volunteer).grades = picklistStringToArray(record[0].grades__c);
-                (user as Volunteer).introductionMethod = record[0].introductionMethod__c;
-                (user as Volunteer).isVolunteerCoordinator = record[0].isVolunteerCoordinator__c;
-                (user as Volunteer).languages = picklistStringToArray(record[0].languages__c);
-                (user as Volunteer).linkedIn = record[0].linkedIn__c;
-                (user as Volunteer).localPostSecondaryInstitutions = picklistStringToArray(
-                    record[0].localPostSecondaryInstitutions__c
-                );
-                (user as Volunteer).locations = picklistStringToArray(record[0].locations__c);
-                (user as Volunteer).postSecondaryTraining = picklistStringToArray(record[0].postSecondaryTraining__c);
-                (user as Volunteer).professionalAssociations = picklistStringToArray(
-                    record[0].professionalAssociations__c
-                );
-                (user as Volunteer).reasonsForVolunteering = picklistStringToArray(record[0].reasonsForVolunteering__c);
-                (user as Volunteer).volunteerDesiredExternalActivities = picklistStringToArray(
-                    record[0].volunteerDesiredExternalActivities__c
-                );
-                (user as Volunteer).volunteerDesiredInternalActivities = picklistStringToArray(
-                    record[0].volunteerDesiredInternalActivities__c
-                );
-            }
-            return user;
+            return salesforceUserToUserModel(record[0]);
         });
 
     return user;
+};
+
+// Retrieve list of volunteers in alphabetical order
+
+export const getVolunteers = async (limit: number, offset: number): Promise<User[]> => {
+    let volunteers: User[] = [];
+    const volunteerUserType: number = 2;
+    await conn.query(
+        `SELECT ${userFields} FROM ${siteUser} WHERE userType__c=${volunteerUserType} ORDER BY Name LIMIT ${limit} OFFSET ${offset}`,
+        function(err, result) {
+            if (err) {
+                return console.error(err);
+            }
+            volunteers = result.records.map(record => salesforceUserToUserModel(record));
+        }
+    );
+
+    return volunteers;
 };
 
 // Update user by ID.
@@ -183,7 +201,7 @@ export const update = async (id: string, user: User): Promise<User> => {
 
     const updatedUser: User = conn
         .sobject(siteUser)
-        .update(createSalesforceUser(user, id), (err: Error, result: any) => {
+        .update(userModelToSalesforceUser(user, id), (err: Error, result: any) => {
             if (err || !result.success) {
                 return console.error(err, result);
             }
@@ -196,7 +214,7 @@ export const update = async (id: string, user: User): Promise<User> => {
 export const create = async (user: User): Promise<string> => {
     const userInfo: { id: string; success: boolean; errors: Error[] } = await conn
         .sobject(siteUser)
-        .create(createSalesforceUser(user), (err: Error, result: any) => {
+        .create(userModelToSalesforceUser(user), (err: Error, result: any) => {
             if (err || !result.success) {
                 return console.error(err, result);
             }
