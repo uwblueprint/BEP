d2cbc309df18646874bfc494dde19840e5129588
Ainley <43100685+AinleyP@users.noreply.github.com>
2020-07-06 20:51:01 -0400

    Event Api (#35)
    * creating event api requests
    
    * adding put and delete requests
    
    * Initial commit for opportunity invites.
    
    * Add inviteRouter to server.ts
    
    * Volunteer Application API (#36)
    
    * Fix for Webpack (#32)
    
    * fix(webpack): remove webpack from package.json
    
    * add interface, service, and router for volunteer application api
    
    * fix getApplicationInfo typos
    
    Co-authored-by: Megan Niu <megan.niu@gmail.com>
    Co-authored-by: Wasiq Mohammad <wasiq.mohammad@uwaterloo.com>
    
    * adding eventRouter to server.ts and fixing double apostrophe
    
    * Add accept & decline functionality to invites.
    
    Co-authored-by: Zi Ming He <zm2he@uwaterloo.ca>
    Co-authored-by: Wasiq Mohammad <wasiq.mohammad@uwaterloo.ca>
    Co-authored-by: Megan Niu <megan.niu@gmail.com>
    Co-authored-by: Wasiq Mohammad <wasiq.mohammad@uwaterloo.com>

---
+++ server/src/users/EventService.ts
@@ -0,0 +1,97 @@
+/**
+ * Data Model Interfaces
+ */
+
+import Event from './EventInterface';
+import { conn } from '../server';
+// import * as express from 'express';
+
+
+const eventApi: string = 'Event__c';
+
+/**
+ * Service Methods
+ */
+
+
+/**
+ * See https://jsforce.github.io/document/ under Query and CRUD for documentation
+ * Field Names of eventApi object in Salesforce can be found by clicking Gear Icon -> Schema Builder -> event__c -> Fields
+ */
+
+
+
+// Basic query for now to retrieve a user based on first name (should be changed to ID in future)
+export const getEventInfo = async (name: string): Promise<Event> => {
+    let eventInfo: Event = conn
+        .sobject(eventApi)
+        .find({
+            Name: name
+        },
+            'Name, isActive__c')
+        .limit(1)
+        .execute(function (err: Error, record: any) {
+            if (err) {
+                return console.error(err);
+            }
+            let event: Event = {
+                eventName: record[0].Name,
+                isActive: false
+            }
+            return event;
+        });
+    console.log(eventInfo);
+    return eventInfo;
+};
+
+export const update = async (id: string, event: Event): Promise<Event> => {
+    let updatedEvent: Event = conn
+        .sobject(eventApi)
+        .update(
+            {
+                eventName: event.eventName,
+                isActive: event.isActive
+            },
+            function (err, ret) {
+                if (err || !ret.success) {
+                    return console.error(err, ret);
+                }
+
+                console.log('Updated Successfully : ' + ret.id);
+            }
+        )
+
+    return updatedEvent;
+};
+
+// create new user object in salesforce with fields 
+// Currently fields do not populate unless hard coded strings are passed into the .create() method, not sure if postman issue or something else
+export const create = async (
+    eventName: string,
+    isActive: boolean
+): Promise<void> => {
+    conn.sobject(event).create(
+        {
+            Name: name,
+            isActive: isActive
+        },
+        function (err: Error, result) {
+            if (err || !result.success) {
+                return console.error(err, result);
+            }
+            console.log('created Event with ID : ' + result.id + result.Name);
+        }
+    );
+};
+
+// Delete a user by name (should be changed to ID in the future once ID field in salesforce is figured out)
+export const remove = async (name: string): Promise<void> => {
+    conn.sobject(eventApi).find({
+        Name: name
+    }).destroy(function (err: Error, result) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log('Deleted Event with ID: ' + result[0].id);
+    })
+}; 
