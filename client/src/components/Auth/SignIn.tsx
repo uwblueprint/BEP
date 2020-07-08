1336d9a0881d3bdbf0af9f7bb57e91950d2e3b3c
Brandon Leung <brandonmichael.kg@gmail.com>
2020-07-08 00:06:52 -0400

    initial structure

--- client/src/components/Auth/SignIn.tsx
+++ client/src/components/Auth/SignIn.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+
 class SignIn extends React.Component {
   constructor(props:any) {
     super(props);
@@ -30,20 +39,51 @@ class SignIn extends React.Component {
 
   render() {
     return (
-        <form>
-            <div>
-                <h4>BEP</h4>
-            </div>
-            <div>
-                Email
-                <br />
-                Password
-                <br />
-                <button type="submit" onClick={this.handleSubmit}>
-                    Log In
-                </button>
-            </div>
+      <div>
+        <form onSubmit={this.handleSubmit} noValidate>
+          <Typography
+            component="h1"
+            variant="h5"
+            align="left"
+          >
+            Login to get started.
+          </Typography>
+          <label>Email</label>
+          <TextField
+            variant="outlined"
+            required
+            fullWidth
+            id="email"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+            <label>Password</label>
+          <TextField
+            variant="outlined"
+            required
+            fullWidth
+            name="password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <Link href="#" variant="body2">
+            Forgot password?
+          </Link>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+          >
+            LOGIN
+          </Button>
+          <Link href="#" variant="body2">
+            Don't have an account? Sign up.
+          </Link>
         </form>
+      </div>
     );
   }
 }
