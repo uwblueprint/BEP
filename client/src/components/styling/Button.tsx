d23d106f1f52af71d4990a0d8e83a730aa453fa0
Megan Niu <megan.niu@gmail.com>
2020-07-30 00:20:39 -0400

    Add styled components (#59)
    * feat(components): add styled components
    
    * feat(components): delete button styling file

--- client/src/components/styling/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { Theme } from "@material-ui/core/styles";
-
-const containedButtonStyle = (theme: Theme) => ({
-  button: {
-    background: theme.palette.primary.dark,
-  },
-});
-
-export { containedButtonStyle };
--- client/src/pages/Requests/TestSection.jsx
+++ client/src/pages/Requests/TestSection.jsx
@@ -7,17 +7,16 @@ import { fetchTestService } from '../../data/services/testServices';
 /* Selectors */
 import { getTests } from '../../data/selectors/testSelector';
 
-/* Components and Styling */
-import Button from '../../components/Button'
-import { withStyles } from '@material-ui/core/styles';
-import { containedButtonStyle } from '../../components/styling/Button';
+/* Components */
+import { Grid } from "@material-ui/core";
+import { ContainedButton, PageHeader, PageBody } from '../../components/index';
 
 const mapStateToProps = state => ({
     users: getTests(state),
 })
 
 const mapDispatchToProps = (dispatch) => ({
-    testing: dispatch(fetchTestService()),
+    testing: () => dispatch(fetchTestService()),
 })
 
 class TestSection extends React.Component {
@@ -28,7 +27,6 @@ class TestSection extends React.Component {
     }
 
     render () {
-        const{classes} = this.props;
         const { users } = this.props;
         console.log('component accessible data ', users);
         const test = users.map((user) => (
@@ -38,16 +36,19 @@ class TestSection extends React.Component {
             </div>
         ))
         return (
-            <div>
-                <Button variant="contained" className={classes.button}>hello</Button>
-                
-                <div>test query is fetching data from the test backend API:</div>
-                <div>
-                    {test}
-                </div>
+            <div style={{ height: "100vh" }}>
+                <Grid container style={{ height:"100%" }}>
+                    <PageHeader header="Test Page Header"/>
+                    <PageBody>
+                        <ContainedButton onClick={()=>console.log(this.props.testing())}>FETCH DATA</ContainedButton>
+                        <div>
+                            {test}
+                        </div>
+                    </PageBody>
+                </Grid>
             </div>
         )
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(withStyles(containedButtonStyle)(TestSection));
+export default connect(mapStateToProps, mapDispatchToProps)(TestSection);
