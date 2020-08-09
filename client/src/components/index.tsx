dbf07b67e9374400fdb2098f08335dc39e59bd76
Faizaan Madhani <fzmadhani@gmail.com>
2020-08-09 13:26:49 -0400

    Remove PageBodyGutter export in components/index.tsx

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -2,10 +2,10 @@ import { ContainedButton, DarkContainedButton, TextButton, Button } from "./Butt
 import Link from "./Link";
 import { OutlinedTextField, TextField } from "./TextField";
 import { ContainedSelect, Select } from "./Select";
-import { PageHeader, PageBody, PageBodyGutter, PageHeaderGutter } from "./Page";
+import { PageHeader, PageBody} from "./Page";
 
 export { ContainedButton, DarkContainedButton, TextButton, Button };
 export { Link };
 export { OutlinedTextField, TextField };
 export { ContainedSelect, Select };
-export { PageHeader, PageBody, PageBodyGutter, PageHeaderGutter };
+export { PageHeader, PageBody };
