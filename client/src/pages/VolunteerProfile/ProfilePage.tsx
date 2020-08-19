d25918fe902c84ba0910acc7e53b40bfc2011ef6
Bruce He <bruce.he.62@gmail.com>
2020-08-19 01:21:37 -0400

    Modify volunteer description for profile and refactoring.

--- client/src/pages/VolunteerProfile/ProfilePage.tsx
+++ client/src/pages/VolunteerProfile/ProfilePage.tsx
@@ -1,20 +1,23 @@
 import React from "react";
+import { PageBody } from "../../components/index";
 import VolunteerProfile from "./VolunteerProfile";
 
 export default function ProfilePage(props: any) {
-    return(
-        <VolunteerProfile
-            email = "anne.boelyn@email.com"
-            firstName = "Anne"
-            lastName = "Boelyn"
-            employmentStatus = "Full Time"
-            jobTitle = "Product Manager"
-            employer = "VoiceHero"
-            linkedIn = "https://www.linkedin.com/anneb"
-            expertiseAreas = {["Admin/Business", "HR"]}
-            sectors = {["Information and Communication Technology"]}
-            postSecondaryTraining = {["University", "Other"]}
-            description="Volunteer description"
-        />
-    );
+  return (
+    <PageBody>
+      <VolunteerProfile
+        email="anne.boelyn@email.com"
+        firstName="Anne"
+        lastName="Boelyn"
+        employmentStatus="Full Time"
+        jobTitle="Product Manager"
+        employer="VoiceHero"
+        linkedIn="https://www.linkedin.com/anneb"
+        expertiseAreas={["Admin/Business", "HR"]}
+        sectors={["Information and Communication Technology"]}
+        postSecondaryTraining={["University", "Other"]}
+        description="Volunteer description"
+      />
+    </PageBody>
+  );
 }
--- client/src/pages/VolunteerProfile/VolunteerProfile.tsx
+++ client/src/pages/VolunteerProfile/VolunteerProfile.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
-import { ContainedButton, PageBody } from "../../components/index";
+import { ContainedButton} from "../../components/index";
 import ProfileCard from "./ProfileCard";
 import ContactCard from "./ContactCard";
 
@@ -20,44 +20,40 @@ class VolunteerProfile extends React.Component<{
 }> {
   render() {
     return (
-      <PageBody>
-        <Grid container spacing={3} direction="column">
-          <Grid item xs={12}>
-            <div style={{ display: "flex", flexDirection: "row" }}>
-              <Typography variant="h1" style={{ fontWeight: "bold" }}>
-                {this.props.firstName} {this.props.lastName}
-              </Typography>
-              <div style={{ flexGrow: 2 }} />
-              <ContainedButton>Request</ContainedButton>
-            </div>
-          </Grid>
-          <Grid item xs={12}>
-            <Typography>
-              {this.props.jobTitle}{" "}
-              {this.props.employer ? `at ${this.props.employer}` : ""}
+      <Grid container spacing={3} direction="column">
+        <Grid item xs={12}>
+          <div style={{ display: "flex", flexDirection: "row" }}>
+            <Typography variant="h1" style={{ fontWeight: "bold" }}>
+              {this.props.firstName} {this.props.lastName}
             </Typography>
-          </Grid>
-          {this.props.description && (
-            <Grid item xs={10}>
-              <Typography variant="body1">{this.props.description}</Typography>
-            </Grid>
-          )}
-          <Grid item xs={12}>
-            <ProfileCard
-              employmentStatus={this.props.employmentStatus}
-              sectors={this.props.sectors}
-              expertiseAreas={this.props.expertiseAreas}
-              postSecondaryTraining={this.props.postSecondaryTraining}
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <ContactCard
-              linkedIn={this.props.linkedIn}
-              email={this.props.email}
-            />
-          </Grid>
+            <div style={{ flexGrow: 2 }} />
+            <ContainedButton>Request</ContainedButton>
+          </div>
         </Grid>
-      </PageBody>
+        <Grid item xs={12}>
+          <Typography variant="h6">
+            {this.props.jobTitle}{" "}
+            {this.props.employer ? `at ${this.props.employer}` : ""}
+          </Typography>
+          {this.props.description && (
+            <Typography variant="body1">{this.props.description}</Typography>
+          )}
+        </Grid>
+        <Grid item xs={12}>
+          <ProfileCard
+            employmentStatus={this.props.employmentStatus}
+            sectors={this.props.sectors}
+            expertiseAreas={this.props.expertiseAreas}
+            postSecondaryTraining={this.props.postSecondaryTraining}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <ContactCard
+            linkedIn={this.props.linkedIn}
+            email={this.props.email}
+          />
+        </Grid>
+      </Grid>
     );
   }
 }
