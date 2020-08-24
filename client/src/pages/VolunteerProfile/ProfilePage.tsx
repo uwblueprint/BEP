8b1504826d6fa4969819f5e25137615a74e03962
Bruce He <bruce.he.62@gmail.com>
2020-08-24 17:26:54 -0400

    Add new ProfilePage test.

--- client/src/pages/VolunteerProfile/ProfilePage.tsx
+++ client/src/pages/VolunteerProfile/ProfilePage.tsx
@@ -4,20 +4,49 @@ import VolunteerProfile from "./VolunteerProfile";
 
 export default function ProfilePage(props: any) {
   return (
-    <PageBody>
       <VolunteerProfile
-        email="anne.boelyn@email.com"
-        firstName="Anne"
-        lastName="Boelyn"
-        employmentStatus="Full Time"
-        jobTitle="Product Manager"
-        employer="VoiceHero"
-        linkedIn="https://www.linkedin.com/anneb"
-        expertiseAreas={["Admin/Business", "HR"]}
-        sectors={["Information and Communication Technology"]}
-        postSecondaryTraining={["University", "Other"]}
-        description="Volunteer description"
+        volunteer={{
+          id: "SDF",
+          email: "abigail.winston@email.com",
+          firstName: "Abigail",
+          followedPrograms: ["program"],
+          isSubscribed: true,
+          password: "password",
+          lastName: "Winston",
+          phoneNumber: "000 000 0000",
+          preferredPronouns: "She/Her",
+          userType: 2,
+          careerDescription: "Sfdsaf",
+          jobTitle: "Product Manager",
+          department: "dept",
+          employer: {
+            address: "111 Waterwoo St, Waterloo ON, N2L L4G",
+            city: "Waterloo",
+            id: "str",
+            name: "VoiceHero",
+            phoneNumber: "000 000 0000",
+            postalCode: "N2L L4G",
+            sectors: [],
+            size: "SDF",
+            socialMedia: [],
+            website: "https://voicehero.org"
+          },
+          employmentStatus: "Full time",
+          expertiseAreas:["Performing Arts/Entertainment", "Agriculture/Food Production"],
+          extraDescription: "",
+          grades: ["9", "10", "11", "12"],
+          introductionMethod: "intro",
+          isVolunteerCoordinator: false,
+          languages: ["French", "Spanish"],
+          localPostSecondaryInstitutions: ["institution"],
+          locations: ["Kitchener", "Waterloo", "Cambridge"],
+          postSecondaryTraining: ["University"],
+          professionalAssociations: [],
+          reasonsForVolunteering: [],
+          shareWithEmployer: false,
+          volunteerDesiredExternalActivities: ["Workplace Tours", "Job Shadowning Opportunities", "Provide Co-op Placements"],
+          volunteerDesiredInternalActivities: ["Career Talks", "Workshops/Presentations", "Mock Interviews & Resume Preparation"]
+        }}
       />
-    </PageBody>
   );
 }
