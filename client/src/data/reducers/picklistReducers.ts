d4ed119363e33dba8dbb49c51c5dcfdc71c89741
meganniu <megan.niu@gmail.com>
2020-08-25 13:08:30 -0400

    style: rename 'user picklist' to 'picklist'

---
+++ client/src/data/reducers/picklistReducers.ts
@@ -0,0 +1,46 @@
+import { FETCH_PICKLIST } from "../actions/actionTypes";
+import { PicklistType } from "../types/picklistTypes";
+import { PicklistsAction } from "../actions/picklistActions";
+
+export interface PicklistsPayload {
+  picklistType: PicklistType;
+  picklist: string[];
+}
+
+export type PicklistState = {
+  [key in PicklistType]: string[];
+};
+
+const initialState: PicklistState = {
+  [PicklistType.coopPlacementMode]: [],
+  [PicklistType.coopPlacementTime]: [],
+  [PicklistType.educatorDesiredActivities]: [],
+  [PicklistType.expertiseAreas]: [],
+  [PicklistType.followedPrograms]: [],
+  [PicklistType.grades]: [],
+  [PicklistType.introductionMethod]: [],
+  [PicklistType.languages]: [],
+  [PicklistType.localPostSecondaryInstitutions]: [],
+  [PicklistType.locations]: [],
+  [PicklistType.postSecondaryTraining]: [],
+  [PicklistType.professionalAssociations]: [],
+  [PicklistType.volunteerDesiredExternalActivities]: [],
+  [PicklistType.volunteerDesiredInternalActivities]: [],
+};
+
+export default function (
+  state: PicklistState = initialState,
+  action: PicklistsAction
+) {
+  switch (action.type) {
+    case FETCH_PICKLIST:
+      return {
+        ...state,
+        [PicklistType[action.payload.picklistType]]:
+          action.payload.picklist,
+      };
+
+    default:
+      return state;
+  }
+}
