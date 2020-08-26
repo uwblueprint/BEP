76178f2a98ba431f6f18c8a643e6893fdef7aa68
meganniu <megan.niu@gmail.com>
2020-08-26 17:09:40 -0400

    fix(opportunity_list): add keys to list items

--- client/src/pages/OpportunityList/OpportunityList.tsx
+++ client/src/pages/OpportunityList/OpportunityList.tsx
@@ -300,13 +300,8 @@ class OpportunityList extends React.Component<
     let filtersSelected = false;
 
     const createOpportunityCard = (event: Event) => (
-      <Grid item>
-        <EventCard
-          key={event.id}
-          event={event}
-          isPastEvent={false}
-          showOwner={false}
-        />
+      <Grid item key={event.id}>
+        <EventCard event={event} isPastEvent={false} showOwner={false} />
       </Grid>
     );
 
