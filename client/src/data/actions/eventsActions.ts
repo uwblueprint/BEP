c8ab399d539ab90b87269eb20ce085411f072ec5
meganniu <megan.niu@gmail.com>
2020-08-28 13:39:35 -0400

    fix(educator_dashboard): show only user's own events

--- client/src/data/actions/eventsActions.ts
+++ client/src/data/actions/eventsActions.ts
@@ -1,25 +1,9 @@
 import {
-  FETCH_EVENTS,
   FETCH_ACTIVE_EVENTS,
   FETCH_PAST_EVENTS,
   UPDATE_EVENT,
-  CHANGE_EVENTS_FILTER,
 } from "./actionTypes";
 import { Event } from "../types/EventTypes";
-
-// export const fetchEvents = (events: any[]) => ({
-//     type: FETCH_EVENTS,
-//     payload: { list: events }
-// })
-
-export const fetchEvents = (
-  events: Event[],
-  userType: number,
-  userId: string
-) => ({
-  type: FETCH_EVENTS,
-  payload: { list: events, userType, userId },
-});
 
 export const fetchActiveEvents = (
   events: Event[],
@@ -43,13 +27,3 @@ export const updateEvent = (event: Event) => ({
   type: UPDATE_EVENT,
   payload: { event },
 });
-
-export const changeFilter = (filter: any) => ({
-  type: CHANGE_EVENTS_FILTER,
-  filter,
-});
-
-export const eventsFilter = {
-  ACTIVE_EVENT: "ACTIVE_EVENT",
-  PAST_EVENT: "PAST_EVENT",
-};
