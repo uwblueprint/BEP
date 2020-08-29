e10e6b433c31db721e39b796f101ea730e33e927
Brandon Leung <33383521+brandonleung@users.noreply.github.com>
2020-08-28 22:43:01 -0400

    navbar (#86)
    * working redirects
    
    * navbar for three user levels
    
    * typing the enum

--- client/src/data/services/authServices.ts
+++ client/src/data/services/authServices.ts
@@ -5,10 +5,10 @@ export function loginService(email: string, password: string) {
   return (dispatch: any) => {
     return login(email, password).then((res: any) => {
       dispatch(loginRequest(res.data));
-      return true;
+      return res.data;
     }).catch((res: any) => {
       dispatch(loginFailed());
-      return false;
+      return null;
     });
   };
 }
