0c0bd28c42f232b3079845644c6380eedc58d244
meganniu <megan.niu@gmail.com>
2020-09-01 23:54:42 -0400

    feat(auth_api): create volunteer

--- server/src/auth/authRouter.ts
+++ server/src/auth/authRouter.ts
@@ -4,8 +4,9 @@ import * as jwt from 'jsonwebtoken';
 
 import * as UserService from './../api/users/UserService';
 import SchoolInterface from '../api/schools/SchoolInterface';
-import { UserType } from '../api/users/UserInterface';
-import EducatorInterface from '../api/users/EducatorInterface';
+import User, { UserType, isUser } from '../api/users/UserInterface';
+import Educator, { isEducator } from '../api/users/EducatorInterface';
+import Volunteer, { isVolunteer } from '../api/users/VolunteerInterface';
 
 const BCRYPT_ROUNDS = 4;
 
@@ -61,50 +62,25 @@ export const authRouter = Express.Router();
 
 authRouter.post('/register', async (req: Express.Request, res: Express.Response) => {
     try {
-        const email: string = req.body.email;
-        const firstName: string = req.body.firstName;
-        const isSubscribed: boolean = req.body.isSubscribed;
-        const password: string = req.body.password;
-        const phoneNumber: string = req.body.phoneNumber;
-        const lastName: string = req.body.lastName;
-        const preferredPronouns: string = req.body.preferredPronouns;
-        const userType: UserType = req.body.userType;
-        const position: string = req.body.position;
-        const school: SchoolInterface = req.body.school;
-        const moreInfo: string[] = req.body.moreInfo;
-        const introductionMethod: string = req.body.introductionMethod;
-        const educatorDesiredActivities: string[] = req.body.educatorDesiredActivities;
+        const user: User = req.body;
+
+        if (!isUser(user) || (!isVolunteer(user) && !isEducator(user))) {
+            throw Error('Input is not a valid user.');
+        }
 
         // Hash Password
-        const hash = await bcrypt.hash(password, BCRYPT_ROUNDS);
-
-        const user: EducatorInterface = {
-            educatorDesiredActivities,
-            email,
-            firstName,
-            introductionMethod,
-            isSubscribed,
-            lastName,
-            moreInfo,
-            password: hash,
-            phoneNumber,
-            position,
-            preferredPronouns,
-            school,
-            userType
-        };
+        const hash = await bcrypt.hash(user.password, BCRYPT_ROUNDS);
+        user.password = hash;
 
         await UserService.create(user).then(response => {
             try {
-                if (response['success']) {
-                    const token = jwt.sign({ email }, process.env.SECRET_KEY, { expiresIn: 5000 });
-                    res.status(200).send({
-                        data: response,
-                        token
-                    });
-                }
+                const token = jwt.sign({ email: user.email }, process.env.SECRET_KEY, { expiresIn: 5000 });
+                res.status(200).send({
+                    id: response,
+                    token
+                });
             } catch (e) {
-                res.send(e.message);
+                res.status(500).send(e.message);
             }
         });
     } catch (e) {
